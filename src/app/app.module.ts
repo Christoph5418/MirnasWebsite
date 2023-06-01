1476430faf9049557665c7374f60287685e61ba9
Chris Fadel <90206880+Christoph5418@users.noreply.github.com>
2023-05-31 22:08:22 -0400

    finiished home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,14 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { MenuPageComponent } from './menu-page/menu-page.component';
-import { OrderOnlinePageComponent } from './order-online-page/order-online-page.component';
 import { PrivatePartiesPageComponent } from './private-parties-page/private-parties-page.component';
+import { ContactUsPageComponent } from './contact-us-page/contact-us-page.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomePageComponent},
   {path: 'menu', component: MenuPageComponent},
-  {path: 'order-online', component: OrderOnlinePageComponent},
   {path: 'private-parties', component: PrivatePartiesPageComponent},
+  {path: 'contact-us', component: ContactUsPageComponent},
 ]
 
 
@@ -23,13 +23,13 @@ const appRoutes: Routes = [
     AppComponent,
     NavBarComponent,
     MenuPageComponent,
-    OrderOnlinePageComponent,
     PrivatePartiesPageComponent,
+    ContactUsPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
     ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],  
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+
+  ngOnInit(): void {
+    AOS.init({
+      offset: 300,
+      duration: 1500
+    });
+  }
 
 }
